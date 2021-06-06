02adae9b3f64f8ec3505a3f8f9364d062a620b24
BlueSpirit48 <hcon241@gmail.com>
2021-06-06 10:10:45 +0300

    some_changes

--- static/js/eventDisp.js
+++ static/js/eventDisp.js
@@ -1,6 +1,7 @@
 const eventTitle = document.querySelector('.eveTitle')
 const eventImg = document.querySelector('.eveimg')
 const eventText = document.querySelector('.textText')
+const dateOf = document.querySelector('.dateOf')
 
 
 fetch('/html/event.html/data')
@@ -10,12 +11,18 @@ fetch('/html/event.html/data')
         let elTitle = document.createElement('h2')
         eventTitle.appendChild(elTitle);
         elTitle.innerHTML = eventObj.title;
+        let elDate = document.createElement('h2')
+        dateOf.appendChild(elDate)
+        if(eventObj.expire_day.length > 3){
+            elDate.innerHTML = eventObj.start_day + ' | ' + eventObj.expire_day
+        }else{
+            elDate.innerHTML = eventObj.start_day
+        }
         let elImg = document.createElement('img');
         eventImg.appendChild(elImg);
         elImg.setAttribute('src',eventObj.img)
         let elText = document.createElement('p')
         eventText.appendChild(elText)
         elText.innerHTML = eventObj.text.toString()
-        //console.log(eventObj)
         
     })
--- static/js/ticket_template.js
+++ static/js/ticket_template.js
@@ -4,7 +4,7 @@ fetch('/html/ticket_template.html/showticket')
     console.log(data);
     document.querySelector('#onoma').innerHTML = data.user_first_name;
     document.querySelector('#epwnimo').innerHTML = data.user_last_name;
-    document.querySelector('#hmeromhnia').innerHTML = data.visitday.toString();
+    document.querySelector('#hmeromhnia').innerHTML = data.visitday.slice(0,data.visitday.indexOf('T'));
     document.querySelector('#kathgoria').innerHTML = data.discount;
     document.querySelector('#ticket_code').innerHTML = data.ticket_code;
     document.querySelector('#email').innerHTML = data.user_mail;
